b728c6cfe237c1f5bb48bc7f9a35747c4b6cac75
punnapavankumar9 <punnapavankumar9@gmail.com>
2023-10-20 21:49:27 +0530

    set webpage title as hostname

--- docker/express/simple/src/app.ts
+++ docker/express/simple/src/app.ts
@@ -1,16 +1,21 @@
-import express, { Express, Application, Request, Response } from 'express';
-import path from 'path';
+import express, { Express, Application, Request, Response } from "express";
+import path from "path";
+import ejs from "ejs";
+import { hostname } from "os";
 
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT || 80;
 
 const app = express();
 
-app.get("", (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, "..", "static", "index.html"));
+app.get("", async (req: Request, res: Response) => {
+  console.log(hostname());
+  res.send(
+    await ejs.renderFile(path.join(__dirname, "..", "static", "index.ejs"), {
+      hostname: hostname(),
+    })
+  );
 });
 
-
 app.listen(PORT, () => {
-    console.log("Application started on port: ", PORT);
-})
-
+  console.log("Application started on port: ", PORT);
+});
