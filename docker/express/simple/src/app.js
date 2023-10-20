edf69859d77f61261218c679060619f9cfc8fbf2
punnapavankumar9 <punnapavankumar9@gmail.com>
2023-10-20 22:24:51 +0530

    migrate from ts -> js

--- docker/express/simple/src/app.js
+++ docker/express/simple/src/app.js
@@ -1,13 +1,17 @@
-import express, { Express, Application, Request, Response } from "express";
-import path from "path";
-import ejs from "ejs";
-import { hostname } from "os";
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const ejs = require('ejs');
+const { hostname } = require('os');
+const path = require('path');
 
 const PORT = process.env.PORT || 80;
 
 const app = express();
+app.use(morgan('combined'));
+app.use(helmet());
 
-app.get("", async (req: Request, res: Response) => {
+app.get("", async (req, res) => {
   console.log(hostname());
   res.send(
     await ejs.renderFile(path.join(__dirname, "..", "static", "index.ejs"), {
